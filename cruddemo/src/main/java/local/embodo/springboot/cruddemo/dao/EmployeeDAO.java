fa4577acafb271aef17f8452e3c41f2077480567
Embodo, Jhoda Louise <jhoda.louise.embodo@accenture.com>
2021-05-19 10:49:37 +0800

    first commit

---
+++ cruddemo/src/main/java/local/embodo/springboot/cruddemo/dao/EmployeeDAO.java
@@ -0,0 +1,18 @@
+package local.embodo.springboot.cruddemo.dao;
+
+import java.util.List;
+
+import local.embodo.springboot.cruddemo.entity.Employee;
+
+public interface EmployeeDAO {
+	
+	public List<Employee> findAll();
+	
+	public Employee findById(int theId);
+	
+	public void save(Employee theEmployee);
+	
+	public void deleteById(int theId);
+	
+
+}
